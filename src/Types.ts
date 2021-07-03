1b2242e783de078b59e40aa3dcd7443515dff78c
Joaquín R. Montes <joaco.rm512@gmail.com>
2021-07-03 10:54:39 -0300

    added env variables and TS types

---
+++ src/Types.ts
@@ -0,0 +1,14 @@
+export type DataRow = {
+  id: string;
+  commerce: string;
+  cuit: number;
+  balance: number;
+  active: boolean;
+  last_sale: Date;
+  concept_1: number;
+  concept_2: number;
+  concept_3: number;
+  concept_4: number;
+  concept_5: number;
+  concept_6: number;
+};
