1edf86bfad10ef56009c1e83902a07ef1c01ffc2
Joaquín R. Montes <joaco.rm512@gmail.com>
2021-07-07 20:27:11 -0300

    Added dummy.json & edited env variables

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,8 @@ module.exports = {
   plugins: ["react", "@typescript-eslint"],
   rules: {
     quotes: ["error", "double"],
+    "no-prototype-builtins": false,
     semi: ["error", "always"],
+    "react/prop-types": [0, {}],
   },
 };
--- src/Types.ts
+++ src/Types.ts
@@ -2,13 +2,28 @@ export type DataRow = {
   id: string;
   commerce: string;
   cuit: number;
-  balance: number;
+  current_balance: number;
   active: boolean;
   last_sale: Date;
-  concept_1: number;
-  concept_2: number;
-  concept_3: number;
-  concept_4: number;
-  concept_5: number;
-  concept_6: number;
+  concepts: number[];
+  // concept_1: number;
+  // concept_2: number;
+  // concept_3: number;
+  // concept_4: number;
+  // concept_5: number;
+  // concept_6: number;
 };
+
+export type Response = {
+  data: DataRow[];
+  page: number;
+  pages: number;
+  rowsPerPage: number;
+  total: number;
+  message_error?: string;
+};
+
+export type FetchToAPI = (
+  query: string,
+  filters: string[]
+) => Promise<Response>;
