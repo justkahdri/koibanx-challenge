cf227aff90ad176ac9e052d679b385ee53b17957
Joaquín R. Montes <joaco.rm512@gmail.com>
2021-07-13 15:13:52 -0300

    Added header image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Heading, Image } from "@chakra-ui/react";
 
 const Header: React.FC = () => {
   return (
@@ -10,7 +10,12 @@ const Header: React.FC = () => {
       p={3}
       width="100%"
     >
-      <Heading>KOIBANX</Heading>
+      <Image
+        src="https://static.wixstatic.com/media/8974d4_1a120064e09245c09fec8d379f4e3b3c~mv2.png/v1/fill/w_360,h_70,al_c,q_85,usm_0.66_1.00_0.01/Productos_KOIBANX-05.webp"
+        fallback={<Heading>KOIBANX</Heading>}
+        height="60px"
+        objectFit="contain"
+      />
     </Box>
   );
 };
